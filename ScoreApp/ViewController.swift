3cf1cb0b9cbff9640d38ca52f1252db58f66870f
HadesSuhen <jjx_wxt@163.com>
2014-09-29 11:31:25 +0800

    1.0
    scoreApp

--- ScoreApp/ViewController.swift
+++ ScoreApp/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didClick(sender: UIButton) {
+        var score = scoreTextField.text.toInt()
+        
+        switch score! {
+            case 0...59:
+                imageView.image = UIImage(named: "delete")
+            break
+            case 60...79:
+                imageView.image = UIImage(named: "right")
+            break
+            case 80...100:
+                imageView.image = UIImage(named: "keditbookmarks")
+            break
+            default:
+            break
+        }
+        
+    }
 
 }
 
